7867852739043911c74b0892de4a49ebc6cd97b9
will <2569933809@qq.com>
2022-08-11 11:31:19 +0800

    backproject init

---
+++ backproject/src/test/java/com/oort/BackprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oort;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
