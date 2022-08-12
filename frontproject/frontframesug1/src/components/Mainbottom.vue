fbc22c4956249fa3aab7a5e40a4685471a89e95a
lingkongninini <96399846+lingkongninini@users.noreply.github.com>
2022-08-12 18:28:56 +0800

    新增登录注册页面布局

--- frontproject/frontframesug1/src/components/Mainbottom.vue
+++ frontproject/frontframesug1/src/components/Mainbottom.vue
@@ -1,12 +1,24 @@
 <template>
-    <div>
-        mainbottom
+    <div class="login">
+        <Login></Login>
     </div>
+
+    <!-- <Register></Register> -->
+    <!-- Register的内容只有 放在Login里面才能正常显示？？？？？？-->
+
 </template>
 
 <script>
+import { Login, Register } from '@/components/index.js'
 
+export default {
+    components: {
+        Login,
+        Register,
+    }
+}
 </script>
 
 <style lang="scss" scoped>
+@import '@/styles/a-my-variables.scss';
 </style>
--- frontproject/frontframesug1/src/components/index.js
+++ frontproject/frontframesug1/src/components/index.js
@@ -5,3 +5,6 @@ export { default as Sidebaractive } from "./Sidebaractive.vue";
 export { default as Maintop } from "./Maintop.vue";
 export { default as Mainbottom } from "./Mainbottom.vue";
 export { default as Sidebarfilterpanel } from "./Sidebarfilterpanel.vue";
+export { default as Login } from "./Login.vue";
+export { default as Register } from "./Register.vue";
+
